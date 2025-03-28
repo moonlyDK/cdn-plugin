792e795cd856f2ac19a5c7789852425e41c9e9eb
KeviinCosmos <KC@KeviinCosmos.com>
2025-03-28 14:13:49 +0100

    Github settings

--- moonly-cdn.php
+++ moonly-cdn.php
@@ -5,7 +5,7 @@
  * Description:       
  * Requires at least: 6.3.0
  * Requires PHP:      7.4
- * Version:           0.0.1
+ * Version:           0.0.2
  * Author:            COS
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
