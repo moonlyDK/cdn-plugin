12ba82191891bf1b95ef151726ed39882acece32
KeviinCosmos <KC@KeviinCosmos.com>
2025-03-28 13:47:56 +0100

    Plugin main files

---
+++ moonly-cdn.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * Plugin Name:       Moonly CDN
+ * Description:       
+ * Requires at least: 6.3.0
+ * Requires PHP:      7.4
+ * Version:           0.0.1
+ * Author:            COS
+ * License:           GPL-2.0-or-later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       moonly_cdn
+ * Website:           
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+$plugin_prefix = 'MOONLYCDN';
+
+// Extract the version number
+$plugin_data = get_file_data(__FILE__, ['Version' => 'Version']);
+
+// Plugin Constants
+define($plugin_prefix . '_DIR', plugin_basename(__DIR__));
+define($plugin_prefix . '_BASE', plugin_basename(__FILE__));
+define($plugin_prefix . '_PATH', plugin_dir_path(__FILE__));
+define($plugin_prefix . '_VER', $plugin_data['Version']);
+define($plugin_prefix . '_CACHE_KEY', 'moonly_cdn-cache-key-for-plugin');
+define($plugin_prefix . '_REMOTE_URL', 'https://viudvikler.dk/wp-content/plugins/hoster/inc/secure-download.php?file=json&download=988&token=84a880fccb79b0f1a67b8a2d98d3f4843d58f26a87b41585360831a632320b04');
+
+require constant($plugin_prefix . '_PATH') . 'inc/update.php';
+
+new MOONLYCDN_DPUpdateChecker(
+	constant($plugin_prefix . '_BASE'),
+	constant($plugin_prefix . '_VER'),
+	constant($plugin_prefix . '_CACHE_KEY'),
+	constant($plugin_prefix . '_REMOTE_URL'),
+);
