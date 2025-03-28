acc5998c62f79c2ca7f7303f6e0cacc993f03865
KeviinCosmos <KC@KeviinCosmos.com>
2025-03-28 14:17:42 +0100

    Plugin launch

--- moonly-cdn.php
+++ moonly-cdn.php
@@ -4,8 +4,8 @@
  * Plugin Name:       Moonly CDN
  * Description:       
  * Requires at least: 6.3.0
- * Requires PHP:      7.4
- * Version:           0.0.2
+ * Requires PHP:      8.1
+ * Version:           1.0.0
  * Author:            COS
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
